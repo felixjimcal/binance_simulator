9029d77c5b9bf1b3c57895fe481ca62026bb035c
felixjcalvo <felixjcalvo@gmail.com>
2022-04-25 11:40:56 +0200

    -

--- simulator.py
+++ simulator.py
@@ -43,9 +43,9 @@ def strategy_tendencies(df, symbl):
         for i, row in df.iterrows():
             # Running out of money
             if my_balance <= 0:
-                return "strategy_tendencies", "RIP", round(my_balance, 2), row.ctmString
+                return "strategy_tendencies", "RIP", round(my_balance, 2), row.ctm_string
 
-            if row['L_BB_L'] or row['C_BB_L']:
+            if row['entry']:
                 size_in_usdt = 1  # FIXED_RISK * my_balance * min_amount * row.close
                 if 0 < size_in_usdt < my_balance and my_balance > (min_amount * row.close) and my_balance > (BALANCE * BALANCE_PILLOW):
                     min_margin = round(0.004 * size_in_usdt, 2)
@@ -56,9 +56,9 @@ def strategy_tendencies(df, symbl):
                         fees = round(((size_in_usdt * 0.04) / 100), 4)
                         buy_price = row.close + fees
                         sl = buy_price - (0.20 * buy_price)
-                        res = list(map(float, row.resistances.replace('[', '').replace(']', '').split(",")))
-                        tp = buy_price + 0.01 + size_in_usdt if not any(ele > buy_price for ele in res) else next(x[1] for x in enumerate(res) if x[1] > buy_price)
-                        last_buys.append((buy_price, sl, tp, size_in_usdt, row.ctmString, row.timestamp, fees))
+                        res = 0  # list(map(float, row.resistances.replace('[', '').replace(']', '').split(",")))
+                        tp = 0  # buy_price + (0.5 * buy_price)  # buy_price + 0.01 + size_in_usdt if not any(ele > buy_price for ele in res) else next(x[1] for x in enumerate(res) if x[1] > buy_price)
+                        last_buys.append((buy_price, sl, tp, size_in_usdt, row.ctm_string, row.timestamp, fees))
                         open_trades += 1
                         my_balance -= size_in_usdt
 
@@ -69,7 +69,7 @@ def strategy_tendencies(df, symbl):
                 size_in_usdt = lb[3]
                 if row.low <= stop_price <= row.high:
                     result = (stop_price - last_buy) * (size_in_usdt / last_buy)
-                    print(last_buys[e])
+                    # print(last_buys[e])
                     if result < 0:
                         losses += result
                     elif result > 0:
@@ -79,7 +79,8 @@ def strategy_tendencies(df, symbl):
                     del last_buys[e]
                     closed_trades += 1
                     continue
-                if row.low <= tp <= row.high or row.high > tp:
+                # if row.low <= tp <= row.high or row.high > tp:
+                if row['RSI_OVER_70']:
                     result = (row.close - last_buy) * (size_in_usdt / last_buy)
                     if result < 0:
                         continue
@@ -91,7 +92,7 @@ def strategy_tendencies(df, symbl):
                     closed_trades += 1
                     continue
         trades = 'OT:', open_trades, 'CT', closed_trades
-        margin_dates = df.ctmString[0], df.ctmString[len(df) - 1]
+        margin_dates = df.ctm_string[0], df.ctm_string[len(df) - 1]
         return 'so', 'Real:', round(my_balance, 2), "Profit:", round(gains, 2), "Wins:", round(wins, 2), "Losses:", round(losses, 2), trades, margin_dates
 
     except Exception as ex:
@@ -103,7 +104,7 @@ def prepare_data(crypto_symbol):
     """
 
     """
-    ohlc = client.klines(crypto_symbol, "1d", **{"limit": 1500})
+    ohlc = client.klines(crypto_symbol, "1d", **{"limit": 1500})  # limit 1500
     ohlc.pop()  # https://binance-docs.github.io/apidocs/futures/en/#kline-candlestick-data
     df_day = pd.DataFrame(ohlc, columns=[TIMESTAMP, OPEN, HIGH, LOW, CLOSE, VOLUME, 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
     df_day = df_day.drop(['close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'], axis=1)
@@ -112,29 +113,29 @@ def prepare_data(crypto_symbol):
     for col in df_day.columns[2:]:
         df_day[col] = pd.to_numeric(df_day[col])
 
-    # df_day['EMA_S'] = round(talib.EMA(df_day.close, 20), 4)
-    # df_day['EMA_M'] = round(talib.EMA(df_day.close, 70), 4)
-    # df_day['EMA_L'] = round(talib.EMA(df_day.close, 100), 4)
+    df_day['EMA_S'] = round(talib.EMA(df_day.close, 20), 4)
+    df_day['EMA_M'] = round(talib.EMA(df_day.close, 70), 4)
+    df_day['EMA_L'] = round(talib.EMA(df_day.close, 100), 4)
 
     # Es interesante plantear un código para detectar picos y divergencias en el RSI
     # el RSI baja y el precio sube en un plazo de tiempo más grande, divergencia
-    # df_day['RSI'] = round(talib.RSI(df_day.close, 14), 4)
-    # df_day['RSI_UNDER_30'] = np.where(df_day.RSI < 30, True, False)
-    # df_day['RSI_OVER_70'] = np.where(df_day.RSI > 70, True, False)
+    df_day['RSI'] = round(talib.RSI(df_day.close, 14), 4)
+    df_day['RSI_UNDER_30'] = np.where(df_day.RSI < 30, True, False)
+    df_day['RSI_OVER_70'] = np.where(df_day.RSI > 90, True, False)
 
     # https://www.youtube.com/watch?v=IeasOiEua-4&t=0s&ab_channel=An%C3%A1lisisT%C3%A9cnicoSt.
-    upper, middle, lower = talib.BBANDS(df_day.close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
-    df_day['BB_U'] = round(upper, 4)
-    df_day['BB_M'] = round(middle, 4)
-    df_day['BB_L'] = round(lower, 4)
-    df_day['L_BB_L'] = np.where(df_day.low <= df_day.BB_L, True, False)
-    df_day['C_BB_L'] = np.where(df_day.close <= df_day.BB_L, True, False)
+    # upper, middle, lower = talib.BBANDS(df_day.close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    # df_day['BB_U'] = round(upper, 4)
+    # df_day['BB_M'] = round(middle, 4)
+    # df_day['BB_L'] = round(lower, 4)
+    # df_day['L_BB_L'] = np.where(df_day.low <= df_day.BB_L, True, False)
 
     # 7, 10, 6, 6
-    # sto_k, sto_d = talib.STOCHRSI(df_day.close, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)
+    # sto_k, sto_d = talib.STOCHRSI(df_day.close, timeperiod=10, fastk_period=7, fastd_period=6, fastd_matype=6)
     # df_day['sto_k'] = round(sto_k, 4)
     # df_day['sto_d'] = round(sto_d, 4)
 
+    # df_day['entry'] = np.where(, True, False)
     print(crypto_symbol, strategy_tendencies(df_day, crypto_symbol))
     # print(crypto_symbol, strategy_(df_day, crypto_symbol))
     # print(crypto_symbol, strategy_a(df_day, crypto_symbol))
@@ -143,6 +144,6 @@ def prepare_data(crypto_symbol):
 if __name__ == "__main__":
     client = Futures(key='<api_key>', secret='<api_secret>')
 
-    symbols = ['DOGEUSDT', 'LUNAUSDT', 'SOLUSDT', 'XMRUSDT', 'ZECUSDT']
+    symbols = ['DOGEUSDT' , 'LUNAUSDT', 'SOLUSDT', 'XMRUSDT', 'ZECUSDT']
     for symbol in symbols:
         prepare_data(symbol)
